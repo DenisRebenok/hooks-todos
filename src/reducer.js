040b0a17cadaa24582257816a5e93830762b46f2
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-02-21 03:20:06 +0200

    Adding Todos and TodoForm Component / "ADD_TODO" case

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,17 @@
+import uuidv4 from 'uuid/v4';
+
 export default function reducer(state, action) {
   switch (action.type) {
+    case 'ADD_TODO':
+      const newTodo = {
+        id: uuidv4(),
+        text: action.payload,
+        complete: false
+      };
+      return {
+        ...state,
+        todos: [...state.todos, newTodo]
+      };
     case 'TOGGLE_TODO':
       const toggleTodos = state.todos.map(t =>
         t.id === action.payload.id
