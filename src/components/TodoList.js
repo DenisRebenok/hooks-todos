10feabb07b1d9e32a4f34fcf3c306830411271e0
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-02-21 07:42:57 +0200

    Delete Request to Remove Todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import TodosContext from '../context';
+import axios from 'axios';
 
 export default function TodoList() {
   const { state, dispatch } = useContext(TodosContext);
@@ -36,7 +37,12 @@ export default function TodoList() {
               />
             </button>
             <button
-              onClick={() => dispatch({ type: 'REMOVE_TODO', payload: todo })}
+              onClick={async () => {
+                await axios.delete(
+                  `https://todos2-1e502.firebaseio.com/todos/${todo.id}.json`
+                );
+                dispatch({ type: 'REMOVE_TODO', payload: todo });
+              }}
             >
               <img
                 src="https://icon.now.sh/delete/8b0000"
